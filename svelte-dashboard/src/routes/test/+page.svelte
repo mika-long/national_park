d7339b579b1a2d1fa140cdf8d6d9d91ab26990cf
Sheng Long <shenglong@u.northwestern.edu>
2026-01-16 16:35:59 -0600

    testing for the map also works ...

--- svelte-dashboard/src/routes/test/+page.svelte
+++ svelte-dashboard/src/routes/test/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    import Test from "$lib/components/Test.svelte";
+    // import Test from "$lib/components/Test.svelte";
+    import TestMap from "$lib/components/TestMap.svelte";
 </script>
 
-<Test />
+<!-- <Test /> -->
+ <TestMap />
